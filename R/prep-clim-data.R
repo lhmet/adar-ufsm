86a3ead9bd8d7b7d8d7f1259ca38cf72c1f59e8c
lhmet <lhmet@ufsm.br>
2018-06-25 18:57:00 -0300

    add geracao de metadados para clima_rs

--- R/prep-clim-data.R
+++ R/prep-clim-data.R
@@ -28,3 +28,23 @@ j <- inner_join(prec_rs, tmax_rs) %>%
   filter(complete.cases(.))
 View(j)
 rio::export(j, "../data/clima-rs.RDS")
+
+
+#-------------------------------------------------------
+# metadata para os dados clima_rs
+excel_file_url <- "http://www.inmet.gov.br/webcdp/climatologia/normais2/imagens/normais/planilhas/1961-1990/Relac_Est_Meteo_NC.xls"
+dest_file_excel <- file.path(tempdir(), "Estacoes-Normal-Climatologica-1961-1990.xls")
+download.file(excel_file_url, dest_file_excel)
+
+source("~/Dropbox/github/my_reps/lhmet/inmetr/data-raw/bdmep_meta_funs.R")
+
+inmet_nc_meta <- read_meteo_stn_xls(dest_file_excel) %>%
+  as_tibble() %>%
+  filter(uf == "RS") %>%
+  select(-state) %>%
+  rename("codigo" = id) %>%
+  mutate(name = NULL, uf = NULL)
+inmet_nc_meta
+
+saveRDS(inmet_nc_meta, "../data/clima_rs_metadata_61_90.RDS")
+
